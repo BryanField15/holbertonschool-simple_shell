42d2324a45c2d2c24ba27102f6e274c5a8421ffe
BryanField15 <6367@holbertonstudents.com>
2023-04-24 21:41:18 -0700

    revert

--- fork.c
+++ fork.c
@@ -10,37 +10,25 @@ int run_command(char *input)
 {
 	pid_t child;
 	int status;
-	char *ls_args[] = {"./hbtn_ls", "/var", NULL};
 
-	if (strcmp(input, "./hbtn_ls /var") == 0)
+	child = fork();
+	if (child == -1)
 	{
-		if (execv("/bin/ls", ls_args) == -1)
+		return (1);
+	}
+	else if (child == 0)
+	{
+		if (_execve(input) == -1)
 		{
-			perror("execv");
 			return (1);
 		}
+		return (0);
 	}
 	else
 	{
-		child = fork();
-		if (child == -1)
+		if (wait(&status) == -1)
 		{
-			return (1);
-		}
-		else if (child == 0)
-		{
-			if (_execve(input) == -1)
-			{
-				return (1);
-			}
-			return (0);
-		}
-		else
-		{
-			if (wait(&status) == -1)
-			{
-				perror("wait");
-			}
+			perror("wait");
 		}
 	}
 	return (0);
