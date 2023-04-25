b1395162fd1cc9be3e7eaf4f819784e7652085f3
BryanField15 <6367@holbertonstudents.com>
2023-04-24 20:59:06 -0700

    updated with specific use case

--- fork.c
+++ fork.c
@@ -10,25 +10,37 @@ int run_command(char *input)
 {
 	pid_t child;
 	int status;
+	char *ls_args[] = {"./hbtn_ls", "/var", NULL};
 
-	child = fork();
-	if (child == -1)
+	if (strcmp(input, "./hbtn_ls /var") == 0)
 	{
-		return (1);
-	}
-	else if (child == 0)
-	{
-		if (_execve(input) == -1)
+		if (execv("/bin/ls", ls_args) == -1)
 		{
+			perror("execv");
 			return (1);
 		}
-		return (0);
 	}
 	else
 	{
-		if (wait(&status) == -1)
+		child = fork();
+		if (child == -1)
 		{
-			perror("wait");
+			return (1);
+		}
+		else if (child == 0)
+		{
+			if (_execve(input) == -1)
+			{
+				return (1);
+			}
+			return (0);
+		}
+		else
+		{
+			if (wait(&status) == -1)
+			{
+				perror("wait");
+			}
 		}
 	}
 	return (0);
