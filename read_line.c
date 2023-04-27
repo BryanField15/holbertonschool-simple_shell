bffbfebe07624dd21f4010af4d7779bc12d0b9d3
BryanField15 <6367@holbertonstudents.com>
2023-04-26 17:22:47 -0700

    removed comments

--- read_line.c
+++ read_line.c
@@ -14,14 +14,11 @@ char *read_line()
 	line = NULL;
 	length = 0;
 	is_read = getline(&line, &length, stdin);
-/*	printf("Return from getline: %lu\n", is_read);*/
 
 	if (is_read == EOF)
 	{
-/*		printf("inside if EOF statement: %s\n", line);*/
 		free(line);
 		return (NULL);
 	}
-/*	printf("Returned line from stdin from getline: %s\n", line);*/
 	return (line);
 }
